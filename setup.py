3775861890863193c20da7924dce665d8cdfec42
Lukas Bahr <lukas.bahr@rwth-aachen.de>
2021-09-06 13:48:38 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(
     name='lena',
-    url='https://github.com/lukasbahr/lena',
+    url='https://github.com/Centre-automatique-et-systemes/lena',
     author='Lukas Bahr',
-    packages=['lena.net', 'lena.datasets', 'lena.observer', 'lena.util'],
-    install_requires=['numpy', 'torch', 'scipy', 'matplotlib', 'torchdiffeq', 'smt', 'pyyaml'],
+    packages=['lena'],
+    install_requires=['numpy', 'torch', 'scipy', 'matplotlib', 'torchdiffeq', 'smt'],
     version='0.01',
     license='MIT',
     description='Implementation of lena',
