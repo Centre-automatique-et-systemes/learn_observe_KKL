Corrected heatmaps to actually use the observer, and added data normalization in the observer model (in the NN directly because simulation and model are mixed and need to be in the physical coordinates...). Base cases working reasonably well

learn_KKL/learner.py
@@ -1,4 +1,5 @@
-import os, sys
+import os
+import sys
 
 import dill as pkl
 import matplotlib.pyplot as plt
@@ -9,33 +10,14 @@ import torch
 import torch.optim as optim
 from smt.sampling_methods import LHS
 from torch.utils.data import DataLoader
+from scipy.interpolate import interp2d
+
+
+from .utils import RMSE, StandardScaler
 
 # Set double precision by default
 torch.set_default_tensor_type(torch.DoubleTensor)
 torch.set_default_dtype(torch.float64)
-
-
-def RMSE(x, y, dim=None):
-    """
-    Compute the root mean squared between x and y along dimension dim.
-
-    Parameters
-    ----------
-    x: torch.tensor
-    y: torch.tensor
-    dim: int
-        Dimension along which to compute the mean.
-
-    Returns
-    -------
-    error: torch.tensor
-        Computed RMSE.
-    """
-    error = torch.nn.functional.mse_loss(x, y, reduction='none')
-    if dim is None:
-        return torch.sqrt(torch.mean(error))
-    else:
-        return torch.sqrt(torch.mean(error, dim=dim))
 
 
 class Learner(pl.LightningModule):
@@ -87,8 +69,14 @@ class Learner(pl.LightningModule):
     Attributes
     ----------
 
-    results_folder : str
+    results_folder: str
         Path of the folder for saving all results.
+
+    scaler_x:
+        Scaler object for x.
+
+    scaler_z:
+        Scaler object for z.
 
     """
 
@@ -107,6 +95,15 @@ class Learner(pl.LightningModule):
         # Data handling
         self.training_data = training_data
         self.validation_data = validation_data
+        if self.method == 'Autoencoder':
+            self.scaler_x = StandardScaler(self.training_data)
+            self.scaler_z = None
+        else:
+            self.scaler_x = StandardScaler(
+                self.training_data[:, :self.model.dim_x])
+            self.scaler_z = StandardScaler(
+                self.training_data[:, self.model.dim_x:])
+        self.model.set_scalers(scaler_x=self.scaler_x, scaler_z=self.scaler_z)
 
         # Optimization
         self.batch_size = batch_size
@@ -120,7 +117,7 @@ class Learner(pl.LightningModule):
         i = 0
         params = os.path.join(os.getcwd(), 'runs', self.model.method)
         if self.model.method == 'Supervised':
-            params += self.method
+            params += '/' + self.method
         while os.path.isdir(os.path.join(params, f"exp_{i}")):
             i += 1
         self.results_folder = os.path.join(params, f"exp_{i}")
@@ -301,21 +298,33 @@ class Learner(pl.LightningModule):
                 pkl.dump(self.model, f, protocol=4)
             print(f'Saved model in {self.results_folder}')
 
-            # Compute heatmap of RMSE(x, x_hat) or RMSE (z, z_hat) if method=T
-            num_samples = 10000
-            sampling = LHS(xlimits=limits)
-            mesh = torch.as_tensor(sampling(num_samples))
-            if self.method == 'Autoencoder':
-                _, mesh_hat = self.model(self.method, mesh)
-            else:
-                mesh_hat = self.model(self.method, mesh)
-            # _, mesh_hat = self.model('Autoencoder', mesh)  # T(T_star(x))
-            error = RMSE(mesh, mesh_hat, dim=1)
-            for i in range(1, mesh.shape[1]):
+            # No control theoretic evaluation of the observer with only T
+            if self.method == 'T':
+                return 0
+
+            # Heatmap of RMSE(x, x_hat) with T_star
+            num_samples = 50000
+            heatmap_data = self.model.generate_data_svl(limits, num_samples)
+            x = heatmap_data[:, :self.model.dim_x]
+            z = heatmap_data[:, self.model.dim_x:]
+            x_hat = self.model('T_star', z)
+            error = RMSE(x, x_hat, dim=1)
+            print(x.shape, error.shape)
+            for i in range(1, x.shape[1]):
+                # https://stackoverflow.com/questions/37822925/how-to-smooth-by-interpolation-when-using-pcolormesh
                 name = 'RMSE_heatmap' + str(i) + '.pdf'
-                plt.scatter(mesh[:, i - 1], mesh[:, i], cmap='jet',
+                plt.scatter(x[:, i - 1], x[:, i], cmap='jet',
                             c=np.log(error.numpy()))
                 cbar = plt.colorbar()
+                cbar.set_label('Log estimation error')
+                # print(x[:, i - 1:i + 1].shape)
+                # plt.imshow(x[:, i - 1:i + 1], cmap='jet',
+                #            c=np.log(error.numpy()))
+                # plt.pcolormesh(x[:, i - 1], x[:, i], np.log(error),
+                #                cmap='jet', shading='gouraud')
+                # cbar = plt.colorbar()
+                # cbar.set_label('Log estimation error')
+                # cbar = plt.colorbar()
                 cbar.set_label('Log estimation error')
                 plt.title('Log of RMSE between ' + r'$x$' + ' and '
                           + r'$\hat{'r'x}$')
@@ -328,9 +337,7 @@ class Learner(pl.LightningModule):
                     plt.show()
                 plt.close('all')
 
-            # Estimation over the test trajectories
-            if self.method == 'T':
-                return 0
+            # Estimation over the test trajectories with T_star
             sampling = LHS(xlimits=limits)
             trajs_init = torch.as_tensor(sampling(nb_trajs)).T
             traj_folder = os.path.join(self.results_folder, 'Test_trajectories')

learn_KKL/luenberger_observer.py
@@ -193,6 +193,12 @@ class LuenbergerObserver(nn.Module):
     recon_lambda : float
         Default set to 1.01. Multiplicator on reconstruction loss.
 
+    scaler_x :
+        Scaler for x in the NN model.
+
+    scaler_z :
+        Scaler for z in the NN model.
+
 
     Methods
     ----------
@@ -253,7 +259,8 @@ class LuenbergerObserver(nn.Module):
     """
 
     def __init__(self, dim_x: int, dim_y: int, method: str = "Autoencoder",
-                 dim_z: int = None, wc: float = 1.):
+                 dim_z: int = None, wc: float = 1., num_hl: int = 5,
+                 size_hl: int = 50, activation=nn.ReLU()):
         super(LuenbergerObserver, self).__init__()
 
         self.method = method
@@ -272,8 +279,9 @@ class LuenbergerObserver(nn.Module):
             raise ValueError('dim_y must be 1 or greater')
 
         # Set observer matrices D and F
+        self.wc = wc
         self.F = torch.ones((self.dim_z, 1))
-        self.D = self.bessel_D(wc=wc)
+        self.D = self.bessel_D(wc=self.wc)
 
         # Model params
         self.device = torch.device(
@@ -281,14 +289,18 @@ class LuenbergerObserver(nn.Module):
         self.recon_lambda = 1.01
 
         # Define encoder and decoder architecture
-        num_hl = 5
-        size_hl = 40
-        act = nn.Tanh()
-
-        self.encoder_layers = self.create_layers(num_hl, size_hl, act,
-                                                 self.dim_x, self.dim_z)
-        self.decoder_layers = self.create_layers(num_hl, size_hl, act,
-                                                 self.dim_z, self.dim_x)
+        # num_hl = 5
+        # size_hl = 40
+        # act = nn.Tanh()
+        self.num_hl = num_hl
+        self.size_hl = size_hl
+        self.activation = activation
+        self.encoder_layers = self.create_layers(
+            self.num_hl, self.size_hl, self.activation, self.dim_x, self.dim_z)
+        self.decoder_layers = self.create_layers(
+            self.num_hl, self.size_hl, self.activation, self.dim_z, self.dim_x)
+        self.scaler_x = None
+        self.scaler_z = None
 
     def f(self, x: torch.tensor):
         return 0
@@ -311,10 +323,12 @@ class LuenbergerObserver(nn.Module):
             'dim_x ' + str(self.dim_x),
             'dim_y ' + str(self.dim_y),
             'dim_z ' + str(self.dim_z),
+            'wc' + str(self.wc),
             'D ' + str(self.D),
             'F ' + str(self.F),
             'encoder' + str(self.encoder_layers),
             'decoder' + str(self.decoder_layers),
+            'method' + self.method,
         ])
 
     def __call__(self, method='Autoencoder', *input):
@@ -344,6 +358,23 @@ class LuenbergerObserver(nn.Module):
         self.h = system.h
         self.u = system.u
         self.u_1 = system.u_1
+
+    def set_scalers(self, scaler_x, scaler_z):
+        """
+        Set the scaler objects for input and output data. Then, the internal
+        NN will normalize every input and denormalize every output.
+
+        Parameters
+        ----------
+        scaler_X :
+            Input scaler.
+
+        scaler_out :
+            Output scaler.
+        """
+
+        self.scaler_x = scaler_x
+        self.scaler_z = scaler_z
 
     def bessel_D(self, wc: float = 1.) -> torch.tensor:
         """
@@ -511,11 +542,7 @@ class LuenbergerObserver(nn.Module):
         data: torch.tensor
             Pairs of (x, z) data points.
         """
-        # if limits[1] < limits[0]:
-        #     raise ValueError(
-        #         'limits[0] must be strictly smaller than limits[1]')
 
-        # limits = np.array([limits, limits])
         sampling = LHS(xlimits=limits)
         mesh = torch.as_tensor(sampling(num_samples))
 
@@ -535,7 +562,8 @@ class LuenbergerObserver(nn.Module):
         y_1[:self.dim_x, :] = data_bw[-1, :self.dim_x, :]
         tq, data_fw = self.simulate_system(y_1, tsim, dt)
 
-        # Data contains (x_i, z_i) pairs in shape [dim_z, number_simulations]
+        # Data contains (x_i, z_i) pairs in shape [dim_x + dim_z,
+        # number_simulations]
         data = torch.transpose(data_fw[-1, :, :], 0, 1)
 
         return data
@@ -643,9 +671,13 @@ class LuenbergerObserver(nn.Module):
         z: torch.tensor
             Computation of observer state vector.
         """
-        # Iterate over encoder layers and compute next input
+        # Normalize input, iterate over encoder layers, denormalize output
+        if self.scaler_x is not None:
+            x = self.scaler_x.transform(x)
         for layer in self.encoder_layers:
             x = layer(x)
+        if self.scaler_z is not None:
+            x = self.scaler_z.inverse_transform(x)
 
         return x
 
@@ -664,9 +696,13 @@ class LuenbergerObserver(nn.Module):
             Computation of state vector estimation driving the
             observer.
         """
-        # Iterate over decoder layers and compute next input
+        # Normalize input, iterate over decoder layers, denormalize output
+        if self.scaler_z is not None:
+            z = self.scaler_z.transform(z)
         for layer in self.decoder_layers:
             z = layer(z)
+        if self.scaler_x is not None:
+            z = self.scaler_x.inverse_transform(z)
 
         return z
 