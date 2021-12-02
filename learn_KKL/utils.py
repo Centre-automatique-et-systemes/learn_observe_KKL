3f538d8088c54cb5811912e2185786664336c7bc
Lukas Bahr <lukas.bahr@rwth-aachen.de>
2021-12-02 15:54:53 +0100

    added cuda support for standard scalar class

--- learn_KKL/utils.py
+++ learn_KKL/utils.py
@@ -133,12 +133,13 @@ def RMSE(x, y, dim=None):
 # https://discuss.pytorch.org/t/pytorch-tensor-scaling/38576
 class StandardScaler:
     def __init__(self, X):
-        self._mean = torch.mean(X, dim=0)
-        self._var = torch.var(X, dim=0, unbiased=False)
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self._mean = torch.mean(X, dim=0).to(device)
+        self._var = torch.var(X, dim=0, unbiased=False).to(device)
         # If var = 0., i.e. values all same, make it 1 so unchanged!
         idx = torch.nonzero(self._var == 0.)
         self._var[idx] += 1.
-        self._scale = torch.sqrt(self._var)
+        self._scale = torch.sqrt(self._var).to(device)
         self.n_samples_seen_ = len(X)
 
     def fit(self, X):
