54486fbb0e0b02838d11bef5eca0bef599876e6b
pierreChauris <pierre.chauris@gmail.com>
2022-07-07 11:45:55 +0200

    test du code supervised_duffing

--- learn_KKL/system.py
+++ learn_KKL/system.py
@@ -24,12 +24,13 @@ in time as well as the phase potrait.
 
     import matplotlib.pyplot as plt
     from learn_KKL.system import RevDuffing
+    import torch
 
     # Get reversed duffing as example system
     system = RevDuffing()
 
     # Simulation params
-    x_0 = torch.tensor([[1.],[2.]])
+    x_0 = torch.transpose(torch.tensor([[1.],[2.]]),0,1)
     tsim = (0, 50)
     dt = 2e-1
 
@@ -38,7 +39,7 @@ in time as well as the phase potrait.
     tq, sol_null = system.simulate(x_0, tsim, dt)
 
     # Plot phase potrait
-    plt.plot(soll_null[:,0], sol_null[:,1])
+    plt.plot(sol_null[:,0], sol_null[:,1])
     plt.show()
 
     # Simulation with chirp input
@@ -46,7 +47,7 @@ in time as well as the phase potrait.
     tq, sol_chirp = system.simulate(x_0, tsim, dt)
 
     # Plot phase potrait
-    plt.plot(soll_chirp[:,0], sol_chirp[:,1])
+    plt.plot(sol_chirp[:,0], sol_chirp[:,1])
     plt.show()
 
     # Plot truth ground data and estimation
